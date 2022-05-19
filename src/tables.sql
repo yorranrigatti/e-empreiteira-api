d68cc49d66795e1ed14f3f2026c3a6eb83f52caa
Bernardo Enock <bernardoenockgm@gmail.com>
2022-05-19 11:07:48 -0300

    feat: Querys Tables stock_products and products_cart

---
+++ src/tables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS products_cart(
+    id BIGSERIAL INTEGER NOT NULL,
+    cart_id VARCHAR(100) NOT NULL,
+    products_id VARCHAR(100) NOT NULL,
+    FOREING KEY (cart_id) REFERENCES cart (id) ON DELETE CASCADE,
+    FOREING KEY (products_id) REFERENCES products (id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS stock_products(
+    id BIGSERIAL INTEGER NOT NULL,
+    sale_price DECIMAL(5,2) NOT NULL,
+    cost_price DECIMAL(5,2) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    mark VARCHAR(100) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ
+)
