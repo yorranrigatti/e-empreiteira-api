3de80a032b7738638803edfaafbbae2f5337502f
christian-bm <christianbm1998@gmail.com>
2022-05-25 06:17:48 -0400

    merge: merged branch feature/orders

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,10 @@ import clientRouter from "./client.routes";
 import sessionsRouter from "./sessions.routes";
 import companiesRouter from "./companies.routes";
 import companyOwnerRouter from "./companyOwner.routes";
+import ordersRoutes from "./orders.routes";
 
 export const appRoutes = (app: Express) => {
+  app.use("/orders", ordersRoutes);
   app.use("/owner", companyOwnerRouter);
   app.use("/clients", clientRouter);
   app.use("/sessions", sessionsRouter);
--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -4,28 +4,28 @@ import OrdersController from "../controllers/orders.controller";
 import verifyOrderIdExistMiddleware from "../middlewares/verifyOrderIdExist.middleware";
 import createOwnerSchema from "../validations/createOwner.validation";
 
-const companyOwnerRouter = Router();
+const ordersRoutes = Router();
 
-companyOwnerRouter.post(
+ordersRoutes.post(
   "",
   expressYupMiddleware({ schemaValidator: createOwnerSchema }),
   OrdersController.store
 );
-companyOwnerRouter.get("", OrdersController.index);
-companyOwnerRouter.get(
+ordersRoutes.get("", OrdersController.index);
+ordersRoutes.get(
   "/:id",
   verifyOrderIdExistMiddleware,
   OrdersController.show
 );
-companyOwnerRouter.patch(
+ordersRoutes.patch(
   "/:id",
   verifyOrderIdExistMiddleware,
   OrdersController.update
 );
-companyOwnerRouter.delete(
+ordersRoutes.delete(
   "/:id",
   verifyOrderIdExistMiddleware,
   OrdersController.delete
 );
 
-export default companyOwnerRouter;
+export default ordersRoutes;
